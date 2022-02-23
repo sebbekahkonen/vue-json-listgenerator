9e1bd42cbdab2abd846f856b8a6847cf7a672ef3
Sebastian Kähkönen <sebastian.kahkonen@knowe.se>
2022-02-23 14:17:26 +0100

    cleaned up code

--- src/entry.esm.js
+++ src/entry.esm.js
@@ -1,24 +1,3 @@
-
-// import * as components from './components/index';
-
-// // install function executed by Vue.use()
-// const install = function installVueListGenerator(Vue) {
-// 	Object.entries(components).forEach(([componentName, component]) => {
-// 		Vue.component(componentName, component);
-// 	});
-// };
-
-
-// // Create module definition for Vue.use()
-// export default install;
-
-// // To allow individual component use, export components
-// // each can be registered via Vue.component()
-// export * from './components/index';
-
-
-
-
 // Import vue component
 // import component from './vue-json-listgenerator.vue';
 import * as components from './components/index';
